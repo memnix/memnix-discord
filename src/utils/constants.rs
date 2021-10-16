1c09f099d38a82ccb623ae93bd0194a7d7f8bf37
Yume <yume.network@protonmail.com>
2021-10-16 21:41:12 +0200

    Update version

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,3 +1,4 @@
-pub static MEMNIXBOT_VERSION: &str = "0.1.0-alpha1.4";
-pub static API_VERSION: &str = "0.1.0-alpha1.1";
+pub static MEMNIXBOT_VERSION: &str = "0.1.0-alpha1.5";
+pub static API_VERSION: &str = "0.1.0-alpha1.3";
 pub static TEST_DECK: u32 = 1;
+// TODO: Add url as constants
