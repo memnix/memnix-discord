2f61f439c3a123e08e8ae0a6f3e8a1a523bc637d
Yume <yume.network@protonmail.com>
2021-10-21 19:37:04 +0200

    New Version

--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -1,3 +1,3 @@
-pub static MEMNIXBOT_VERSION: &str = "0.1.0-alpha1.6";
-pub static API_VERSION: &str = "0.1.0-alpha2.0";
+pub static MEMNIXBOT_VERSION: &str = "0.1.0-alpha1.7";
+pub static API_VERSION: &str = "0.1.0-alpha2.1";
 // pub static TEST_DECK: u32 = 1;
